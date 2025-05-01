d9fc2492cbbab988f84615902872bb483816cb3d
MissionLogBlackTiger <knowledge.zone6869@gmail.com>
2025-05-01 15:57:22 +0700

    Setup base website

---
+++ docs/javascript.js
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
