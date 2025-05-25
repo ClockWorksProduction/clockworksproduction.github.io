3c3c599e3f71fd8fd792ce5138bd1278c7877d35
knowledge.zone6869 <knowledge.zone6869@gmail.com>
2025-05-25 08:16:05 +0000

    modified:   .idx/dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }: {
  channel = "stable-24.05";
- packages = 
- env = {};
- idx = \{
- extensions = \[
- \];
- previews = \{
- enable = true;
- previews = \{
- web = \{
- command = \["python3", "-m", "http.server", "$PORT"\];
- manager = "web";
- env = \{
- PORT = "$PORT";
- \};
- \};
- \};
- \};
- \};
-\}
+ packages = [];
+  env = {};
+  idx = {
+    extensions = [
+    ];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["python3", "-m", "http.server", "$PORT"];
+          manager = "web";
+          env = {
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+  };
+}
