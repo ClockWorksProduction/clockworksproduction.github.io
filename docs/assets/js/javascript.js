cee4cf166898c6a2fd5b4244b4baebd50c248832
ClockWork Production <136562893+MissionLogBlackTiger@users.noreply.github.com>
2025-12-08 18:40:31 +0700

    keep only what work

---
+++ docs/assets/js/javascript.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// this one is jut to wait for the page to load
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  
+}
